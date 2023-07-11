62b76b4f25e6b8c202f44f9bcf914038e666e724
MuhammadiyevaSevinch <muhammadiyeva133@gmail.com>
2023-07-11 16:25:21 +0500

    gradle

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,17 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-         //here magic will happen
+        if (str == null || str.length() == 0) {
+            return false;
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (!Character.isDigit(c)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
